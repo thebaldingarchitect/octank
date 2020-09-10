e93b25e3b3ab76068f7ecf64373e25939c68538d
Tay <tyet@f8ffc247f36a.ant.amazon.com>
2020-09-10 08:54:29 +0800

    DB support

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,64 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
+// Load the AWS SDK
+var AWS = require('aws-sdk'),
+    region = "ap-southeast-1",
+    secretName = "octankDBpassword",
+    secret,
+    decodedBinarySecret;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        if (err.code === 'DecryptionFailureException')
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InternalServiceErrorException')
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidParameterException')
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidRequestException')
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'ResourceNotFoundException')
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+    }
+    else {
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+            secret = JSON.parse(secret)
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
+        }
+    }
+    console.log("secret is "+secret.password);
+    if (secret==null || secret=='') {
+      secret.password=dbConfig.PASSWORD
+    }
+    
+    
 var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
+  password: secret.password,
   database: dbConfig.DB
 });
 
 module.exports = connection;
+});
